13a0276a453f43e09aca3838aaae4768bcd8f4b8
Olusegun Okin <olusegun@cashenvoy.com>
2021-03-04 10:25:56 +0000

    Changed container entry point for Staging

--- migrate-db.sh
+++ migrate-db.sh
@@ -2,4 +2,8 @@
 
 cd /var/www/html
 php artisan migrate --force --no-interaction -vvv
-kill 1
+if [ $? != 0 ]; then 
+  kill -6 $(ps -ef | grep /usr/bin/supervisord | grep -v grep | awk '{print $2}')
+else 
+  kill $(ps -ef | grep /usr/bin/supervisord | grep -v grep | awk '{print $2}')
+fi
