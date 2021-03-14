11ae9c04351f66a47662fe1437709d04c62c2140
Olusegun Okin <olusegun@cashenvoy.com>
2021-03-14 13:41:57 +0000

    Updated migration script to install passport only when required

--- migrate-db.sh
+++ migrate-db.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-cd /var/www/html
+cd /var/www/html || exit 1
 php artisan migrate --force --no-interaction -vvv
 if [ $? != 0 ]; then 
+  echo "DB migration error. Exiting with error."
   exit 1
-else 
+fi
+
+cd storage || exit 1
+privkey=oauth-private.key
+pubkey=oauth-public.key
+if [ -f "$privkey" ] && [ -f "$pubkey" ]; then
+  echo "Passport key files already exist. No need to run php artisan passport:install."
   exit 0
+else
+  php artisan passport:install --force
+  if [ $? != 0 ]; then 
+    echo "Passport install error. Exiting with error."
+    exit 1
+  fi
 fi
