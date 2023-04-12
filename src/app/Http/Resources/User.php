8b213eb1a6d6321d34ff022197e738252dedb03e
Omiwale Rashed <talk2omis@gmail.com>
2023-04-12 18:19:25 +0100

    password reset

--- src/app/Http/Resources/User.php
+++ src/app/Http/Resources/User.php
@@ -15,15 +15,9 @@ class User extends JsonResource
 
     public function toArray($request){
       return [
-        'id' => $this->id,
         'first_name' => $this->first_name,
         'last_name' => $this->last_name,
         'email' => $this->email,
-        'email_verified_at' => $this->email_verified_at,
-        'phone_number' => $this->phone_number,
-        'phone_number_verified' => $this->phone_number_verified,
-        'created_at' => $this->created_at,
-        'updated_at' => $this->updated_at
       ];
     }
 }
